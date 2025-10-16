59fad577c5bc2df6341f19f3aeda31d11dd44338
Cal Seagram <143629409+seagram@users.noreply.github.com>
2025-10-15 21:19:13 -0400

    refactor: migrate k8s manifests to terraform

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -11,6 +11,34 @@ terraform {
   }
 }
 
+locals {
+  apps_path = "${path.root}/../kubernetes/apps"
+
+  namespaces = {
+    for f in fileset(local.apps_path, "*/") :
+    trimsuffix(f, "/") => trimsuffix(f, "/")
+  }
+  manifests = {
+    for f in fileset(local.apps_path, "**/*.{yml,yaml}") :
+    f => "${local.apps_path}/${f}"
+  }
+}
+
+resource "kubernetes_namespace" "namespaces" {
+  for_each = local.namespaces
+
+  metadata {
+    name = each.value
+  }
+}
+
+resource "kubernetes_manifest" "manifests" {
+  for_each = local.manifests
+
+  manifest = yamldecode(file(each.value))
+}
+
+
 resource "kubernetes_namespace" "tailscale" {
     metadata {
         name = "tailscale"
