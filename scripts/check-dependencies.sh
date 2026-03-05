c34d894bb8f98910adaf17710bb69bee8f664495
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-03-05 15:24:25 -0500

    feat: add helmfile as dependency

--- scripts/check-dependencies.sh
+++ scripts/check-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-for cmd in terraform ansible kubectl talosctl helm; do
+for cmd in terraform ansible kubectl talosctl helm helmfile; do
     if ! command -v $cmd &> /dev/null; then
         echo "$cmd not found!"
         exit 1
