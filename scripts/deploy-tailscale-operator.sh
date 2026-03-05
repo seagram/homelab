021e9eb288fab74a15a0d87f09c85f7148edcf26
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-03-05 18:07:37 -0500

    fix: tailscale directory path in deployment script

--- scripts/deploy-tailscale-operator.sh
+++ scripts/deploy-tailscale-operator.sh
@@ -9,6 +9,6 @@ export TAILSCALE_OAUTH_CLIENT_ID
 TAILSCALE_OAUTH_CLIENT_SECRET=$(terraform output -raw tailscale_oauth_key)
 export TAILSCALE_OAUTH_CLIENT_SECRET
 
-cd ../kubernetes/
+cd ../kubernetes/tailscale/
 
 helmfile apply
