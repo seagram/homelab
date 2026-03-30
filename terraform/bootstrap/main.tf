07b68fde71658eb97331fca89e8e7f5ad34425bf
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-03-30 17:21:14 -0400

    fix: remove bucket versioning

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -4,9 +4,3 @@ resource "cloudflare_r2_bucket" "terraform_state" {
   location      = "enam" # Eastern North America
   storage_class = "Standard"
 }
-
-resource "cloudflare_r2_bucket_versioning" "terraform_state" {
-  account_id = var.cloudflare_account_id
-  bucket_name = cloudflare_r2_bucket.terraform_state.name
-  enabled = true
-}
