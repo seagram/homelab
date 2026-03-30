f1de3e2c494a06cbc9f76ab1c098e4ce04b6ca3e
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-03-30 17:01:22 -0400

    feat: add cloudflare terraform provider

--- terraform/bootstrap/provider.tf
+++ terraform/bootstrap/provider.tf
@@ -4,10 +4,17 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
   }
 }
 
-
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
 }
