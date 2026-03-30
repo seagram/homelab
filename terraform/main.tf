027ed145c665fae2e797f6c1ec16a600cfeb6405
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-03-30 14:56:51 -0400

    fix: remove talos dependency from cloudflare terraform module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,12 @@ module "tailscale" {
   source = "./modules/tailscale"
 }
 
+module "cloudflare" {
+  source                     = "./modules/cloudflare"
+  cloudflare_zone_id         = var.cloudflare_zone_id
+  tailscale_magic_dns_domain = var.tailscale_magic_dns_domain
+}
+
 module "proxmox" {
   depends_on            = [module.tailscale]
   source                = "./modules/proxmox"
@@ -23,11 +29,4 @@ module "talos" {
   talos_version         = var.talos_version
   tailscale_tailnet_key = module.tailscale.tailscale_tailnet_key
   installer_image_url   = module.proxmox.installer_image_url
-}
-
-module "cloudflare" {
-  depends_on                 = [module.talos]
-  source                     = "./modules/cloudflare"
-  cloudflare_zone_id         = var.cloudflare_zone_id
-  tailscale_magic_dns_domain = var.tailscale_magic_dns_domain
 }
