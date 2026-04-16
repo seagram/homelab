17be97325946c42f345165bc1defb9c0fb8d3953
Cal Seagram <143629409+seagram@users.noreply.github.com>
2026-04-16 13:14:11 -0400

    refactor: use aws kms for sops encryption key

--- bootstrap/terraform/outputs.tf
+++ bootstrap/terraform/outputs.tf
@@ -9,3 +9,12 @@ output "s3_bucket_region" {
 output "s3_bucket_arn" {
   value = aws_s3_bucket.terraform_state.arn
 }
+
+output "sops_kms_key_arn" {
+  value = aws_kms_key.sops.arn
+}
+
+output "sops_kms_alias_arn" {
+  description = "ARN of SOPS KMS alias used in .sops.yaml"
+  value       = aws_kms_alias.sops.arn
+}
